4888bfa015c6ab758b032393ae016ea293af5bb7
vicchiam <vicchiam@ono.com>
2015-07-09 21:04:50 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,13 @@
+//GET /quizes/question
+exports.question=function(req,res){
+    res.render("quizes/question",{pregunta: "Capital de Italia"});
+};
+
+exports.answer=function(req,res){
+    if(req.query.respuesta.match(/roma/i)){
+        res.render("quizes/answer",{respuesta: "Correcto!!"});
+    }
+    else{
+        res.render("quizes/answer",{respuesta: "Incorrecto!!"});
+    }
+}
