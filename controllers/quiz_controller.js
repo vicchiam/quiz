375c7d181817ce75847de772a116cdc9a1be3515
vicchiam <vicchiam@ono.com>
2015-07-24 15:17:02 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,10 +2,23 @@ var models=require('../models/models.js')
 
 //GET /quizes/question
 
+exports.load=function(req,res,next,quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz=quiz;
+				next();
+			}
+			else{
+				next(new Error("No existe quizID="+quizId));
+			}
+		}
+	).catch(function(error){next(error);});
+}
 
 exports.show=function(req,res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render("quizes/show",{quiz:quiz});
+		res.render("quizes/show",{quiz:req.quiz});
 	});	
 }
 
@@ -16,20 +29,19 @@ exports.question=function(req,res){
 };
 
 exports.answer=function(req,res){
-    	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta===quiz.respuesta){
-		        res.render("quizes/answer",{quiz:quiz,respuesta: "Correcto!!"});
-		}
-    		else{
-        		res.render("quizes/answer",{quiz:quiz, respuesta: "Incorrecto!!"});
-    		}
-	});
-}
+	var resultado="Incorrecto!!";
+	if(req.query.respuesta===req.quiz.respuesta){
+		resultado="Correcto!!";
+	}
+	res.render("quizes/answer",{quiz:req.quiz,respuesta: resultado});
+};
 
 exports.index=function(req,res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render("quizes/index.ejs",{quizes:quizes});
-	});
+	models.Quiz.findAll().then(
+		function(quizes){
+			res.render("quizes/index.ejs",{quizes:quizes});
+		}
+	).catch(function(error){next(error);});
 };
 
 exports.author=function(req,res){
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ router.get('/', function(req, res) {
 
 //router.get("/quizes/question", quizController.question);
 //router.get("/quizes/answer", quizController.answer);
+
+router.param("quizId",quizController.load);
 router.get("/quizes", quizController.index);
 router.get("/quizes/:quizId(\\d+)",quizController.show);
 router.get("/quizes/:quizId(\\d+)/answer",quizController.answer);
