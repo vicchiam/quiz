2bcc8949d123a324e4e480dc7f283d545b65f481
vicchiam <vicchiam@ono.com>
2015-08-02 16:53:16 +0200

    Validacion

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,7 +18,7 @@ exports.load=function(req,res,next,quizId){
 
 exports.show=function(req,res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render("quizes/show",{quiz:req.quiz});
+		res.render("quizes/show",{quiz:req.quiz,errors: []});
 	});	
 }
 
@@ -33,13 +33,19 @@ exports.answer=function(req,res){
 	if(req.query.respuesta===req.quiz.respuesta){
 		resultado="Correcto!!";
 	}
-	res.render("quizes/answer",{quiz:req.quiz,respuesta: resultado});
+	res.render("quizes/answer",
+		{
+			quiz:req.quiz,
+			respuesta: resultado,
+			errors: []
+		}
+	);
 };
 
 exports.index=function(req,res){
 	models.Quiz.findAll().then(
 		function(quizes){
-			res.render("quizes/index.ejs",{quizes:quizes});
+			res.render("quizes/index.ejs",{quizes:quizes,errors:[]});
 		}
 	).catch(function(error){next(error);});
 };
@@ -53,15 +59,25 @@ exports.new=function(req,res){
 		{pregunta:"", respuesta:""}
 	);
 
-	res.render("quizes/new",{quiz:quiz});
+	res.render("quizes/new",{quiz:quiz,errors:[]});
 };
 
 exports.create=function(req,res){
 	var quiz=models.Quiz.build(req.body.quiz);
-
-	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
-		res.redirect("/quizes");
-	});
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if(err){
+				res.render("quizes/new",{quiz: quiz, errors: err.errors});
+			}
+			else{
+				quiz
+				.save({fields:["pregunta","respuesta"]})
+				.then(function(){res.redirect("/quizes")})
+			}
+		}
+	);
 };	
 
 
--- models/quiz.js
+++ models/quiz.js
@@ -2,7 +2,15 @@ var sequelize=require("sequelize");
 
 module.exports=function(sequelize,DataTypes){
 	return sequelize.define('Quiz',
-		{pregunta: DataTypes.STRING,
-		 respuesta: DataTypes.STRING,		
-		});
+		{
+			pregunta: {
+				type: DataTypes.STRING,
+				validate: { notEmpty: {msg:"Falta pregunta"}}
+			},
+			respuesta: { 
+				type: DataTypes.STRING,
+				validate: { notEmpty: {msg:"Falta respuesta"}}
+			}		
+		}
+	);
 }
