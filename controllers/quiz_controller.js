8e89ed3c99d0c2fcbcf6b1a54de7f9b397dfaee5
vicchiam <vicchiam@ono.com>
2015-08-02 03:54:04 +0200

    Crear Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -46,4 +46,22 @@ exports.index=function(req,res){
 
 exports.author=function(req,res){
     res.render("author",{});
-}
+};
+
+exports.new=function(req,res){
+	var quiz=models.Quiz.build(
+		{pregunta:"", respuesta:""}
+	);
+
+	res.render("quizes/new",{quiz:quiz});
+};
+
+exports.create=function(req,res){
+	var quiz=models.Quiz.build(req.body.quiz);
+
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
+		res.redirect("/quizes");
+	});
+};	
+
+
