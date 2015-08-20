09cf30c195fed169fb229a3b132fd6c9f54d88fb
Victor Chisvert <vicchiam@ono.com>
2015-08-20 02:59:40 +0200

    Publicar comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,10 +1,26 @@
  var models = require("../models/models.js");
 
- exports.new=function(req,res){
-     res.render("comments/new.ejs",{quizid: req.params.quizId, errors: []});
- };
+exports.load=function(req,res,next,commentId){
+    models.Comment.find({
+        where:{
+            id:Number(commentId)
+        }
+    }).then(function(comment){
+        if(comment){
+            req.comment=comment;
+            next();
+        }
+        else{
+            next(new Error("No existe commentId="+commentId));
+        }
+    }).catch(function(error){next(error)});
+};
 
- exports.create=function(req,res){
+exports.new=function(req,res){
+    res.render("comments/new.ejs",{quizid: req.params.quizId, errors: []});
+};
+
+exports.create=function(req,res){
      var comment=models.Comment.build(
          {
              texto: req.body.comment.texto,
@@ -34,3 +50,10 @@
     )
     .catch(function(error){next(error)});
 };
+
+exports.publish=function(req,res){
+    req.comment.publicado=true;
+    req.comment.save({fields:["publicado"]})
+    .then(function(){ res.redirect("/quizes/"+req.params.quizId); })
+    .catch(function(error){ next(error); });
+}
--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,40 @@ var router = express.Router();
 
 var quizController=require("../controllers/quiz_controller");
 var commentController=require("../controllers/comment_controller");
+var sessionController=require("../controllers/session_controller");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz', errors:{} });
+    res.render('index', { title: 'Quiz', errors:{} });
 });
 
 //router.get("/quizes/question", quizController.question);
 //router.get("/quizes/answer", quizController.answer);
 
-router.param("quizId",quizController.load);
+router.param("quizId", quizController.load);
+router.param("commentId", commentController.load);
+
 router.get("/quizes", quizController.index);
 router.get("/quizes/:quizId(\\d+)",quizController.show);
 router.get("/quizes/:quizId(\\d+)/answer",quizController.answer);
-router.get("/quizes/new",quizController.new);
-router.post("/quizes/create",quizController.create);
-router.get("/quizes/:quizId(\\d+)/edit",quizController.edit);
-router.get("/author",quizController.author);
-router.put("/quizes/:quizId(\\d+)",quizController.update);
-router.delete("/quizes/:quizId(\\d+)",quizController.destroy);
 
+router.get("/quizes/new",sessionController.loginRequired, quizController.new);
+router.post("/quizes/create",sessionController.loginRequired, quizController.create);
+router.get("/quizes/:quizId(\\d+)/edit",sessionController.loginRequired, quizController.edit);
+router.put("/quizes/:quizId(\\d+)",sessionController.loginRequired, quizController.update);
+router.delete("/quizes/:quizId(\\d+)",sessionController.loginRequired, quizController.destroy);
+
+router.get("/author",quizController.author);
+
+//Rutas de comentarios
 router.get("/quizes/:quizId(\\d+)/comments/new", commentController.new);
 router.post("/quizes/:quizId(\\d+)/comments/", commentController.create);
+router.get("/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish",
+    sessionController.loginRequired, commentController.publish);
+
+//Rutas de session
+router.get("/login", sessionController.new);
+router.post("/login", sessionController.create);
+router.get("/logout", sessionController.destroy);
 
 module.exports = router;
